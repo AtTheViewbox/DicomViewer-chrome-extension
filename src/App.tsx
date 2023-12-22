b157f0bd31f76eba9386faa878c2bc1b99c80943
Michael Fei <46078711+mfei1225@users.noreply.github.com>
2023-12-21 18:07:59 -0800

    Main (#1)
    * commit
    
    * comp
    
    * content script
    
    * content script

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,17 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import Button from '@mui/material/Button';
+import HeaderComp from './Components/HeaderComp';
+import Divider from '@mui/material/Divider';
+import {useChromeStorageLocal} from 'use-chrome-storage';
+
 
 function App() {
+  const [value, setValue, isPersistent, error, isInitialStateResolved] = useChromeStorageLocal('test', "init");
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <HeaderComp></HeaderComp>
+      <Divider />
+      <Button variant="outlined">Outlined</Button>
+      <div>Persisted in chrome.storage.local: {value.toString()}</div>
     </div>
   );
 }
